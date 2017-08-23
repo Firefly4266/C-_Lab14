89768736eb424aae7a5eacc04bef13ae7e00c460
Firefly4266 <gaines.johnp@gmail.com>
2017-08-23 13:58:14 -0700

    First half of lab

---
+++ ConvertTime/Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Time
+{
+    private int hour;
+    private int minute;
+    public Time(int hr, int min)
+    {
+        hour = hr;
+        minute = min;
+    }
+    public static explicit operator string(Time time)
+    {
+        char[] spaces = { ':' };
+        string[] elements = 
+    }
+
+}
+   
+
